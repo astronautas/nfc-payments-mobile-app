aaa659f1cd036577754abb2f37368c7ce73af80c
Rytis Kaplūnas <rytkaplunas@gmail.com>
2017-05-23 23:01:59 +0300

    Working registration

--- nfc-app/nfc-app/RegistrationActivity.cs
+++ nfc-app/nfc-app/RegistrationActivity.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Util;
 
 namespace nfc_app
 {
@@ -19,10 +20,40 @@ namespace nfc_app
         private EditText _edtPasswordInput;
         private EditText _edtPasswordConfimInput;
         private Button _btnCreateAccount;
+
+        private string _tag = "_myapp";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Account);
+
+            _btnCreateAccount = FindViewById<Button>(Resource.Id.btnCreateAccount); //What todo?
+            _btnCreateAccount.Click += (o, e) => CreateAccount();
+
+            _edtEmailInput = FindViewById<EditText>(Resource.Id.edtEmailInput);
+            _edtPasswordInput = FindViewById<EditText>(Resource.Id.edtPasswordInput);
+            _edtPasswordConfimInput = FindViewById<EditText>(Resource.Id.edtPasswordConfirmInput);
         }
+
+        protected async void CreateAccount()
+        {
+            string email = _edtEmailInput.Text == string.Empty ? "-" : _edtEmailInput.Text;
+            string password = _edtPasswordInput.Text == string.Empty ? "-" : _edtPasswordInput.Text;
+            string passwordConfirmation = _edtPasswordConfimInput.Text == string.Empty ? "-" : _edtPasswordConfimInput.Text;
+            //check the input
+            string json = string.Format("{{ \"user\": {{ \"email\":\"{0}\", \"password\":\"{1}\",  \"password_confirmation\":\"{2}\"}} }}", email, password, passwordConfirmation);
+            try
+            {
+                string response = await Http.Request("https://thawing-ocean-8598.herokuapp.com/register", json);
+                StartActivity(typeof(UserMainActivity));
+            }
+            catch (Exception ex)
+            {
+                Log.Warn(_tag, ex.Message);
+                //show message window that it failed
+            }
+        }
+
     }
 }

af0faeddd41d83e9a7036329d514977c497159a5
Tomaš Monkevič <Toma Monkevi>
2017-05-23 13:36:26 +0300

    Login screen added

--- nfc-app/nfc-app/Resources/Resource.Designer.cs
+++ nfc-app/nfc-app/Resources/Resource.Designer.cs
@@ -64,10 +64,28 @@ namespace nfc_app
 		{
 			
 			// aapt resource value: 0x7f060001
-			public const int menu_settings = 2131099649;
+			public const int emailInput = 2131099649;
+			
+			// aapt resource value: 0x7f060003
+			public const int loginButton = 2131099651;
 			
 			// aapt resource value: 0x7f060000
-			public const int textView = 2131099648;
+			public const int loginText = 2131099648;
+			
+			// aapt resource value: 0x7f060007
+			public const int menu_settings = 2131099655;
+			
+			// aapt resource value: 0x7f060005
+			public const int noAccText = 2131099653;
+			
+			// aapt resource value: 0x7f060002
+			public const int passwordInput = 2131099650;
+			
+			// aapt resource value: 0x7f060004
+			public const int signupButton = 2131099652;
+			
+			// aapt resource value: 0x7f060006
+			public const int textView = 2131099654;
 			
 			static Id()
 			{
@@ -83,7 +101,10 @@ namespace nfc_app
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Login = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{

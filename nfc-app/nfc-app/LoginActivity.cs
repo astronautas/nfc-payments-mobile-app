08ae3ef6e97bf81b20dd80303b131ce750891300
Tomaš Monkevič <Toma Monkevi>
2017-05-23 17:54:18 +0300

    Http request for login

--- nfc-app/nfc-app/LoginActivity.cs
+++ nfc-app/nfc-app/LoginActivity.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.IO;
 
 using Android.App;
 using Android.Content;
@@ -9,16 +12,56 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Util;
 
 namespace nfc_app
 {
-    [Activity(Label = "Prisijungimas", MainLauncher = false)]
+    [Activity(MainLauncher = true)]
     class LoginActivity : Activity
     {
+        private Button _createUserButton;
+        private Button _loginButton;
+        private EditText _emailInput;
+        private EditText _passwordInput;
+
+        private string _tag = "_myapp";
+        private static readonly HttpClient client = new HttpClient();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Log.Warn(_tag, "this is an info message");
             base.OnCreate(savedInstanceState);
             this.SetContentView(Resource.Layout.Login);
+
+            _createUserButton = FindViewById<Button>(Resource.Id.signupButton);
+            _createUserButton.Click += (o, e) => StartActivity(typeof(RegistrationActivity));
+
+            _loginButton = FindViewById<Button>(Resource.Id.loginButton);
+            _loginButton.Click += (o, e) => Login();
+        }
+
+        protected void Login()
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://thawing-ocean-8598.herokuapp.com/login");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{ \"user\": { \"email\":\"dorkx@gmail.com\", \"password\":\"123123123\"} }";
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Log.Info(_tag, "Response string: " + result);
+                Log.Warn(_tag, "Opa");
+            }
         }
     }
 }

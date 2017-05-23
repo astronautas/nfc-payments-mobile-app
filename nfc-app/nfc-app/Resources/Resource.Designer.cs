d09a672ece8e1b6ee512f15802a8ffc87124dbc8
Rytis Kaplūnas <rytkaplunas@gmail.com>
2017-05-23 14:37:41 +0300

    Resolved conflicts and added RegisterActivity

--- nfc-app/nfc-app/Resources/Resource.Designer.cs
+++ nfc-app/nfc-app/Resources/Resource.Designer.cs
@@ -67,7 +67,6 @@ namespace nfc_app
 			public const int btnCreateAccount = 2131099652;
 			
 			// aapt resource value: 0x7f060001
-
 			public const int edtEmailInput = 2131099649;
 			
 			// aapt resource value: 0x7f060003
@@ -77,36 +76,31 @@ namespace nfc_app
 			public const int edtPasswordInput = 2131099650;
 			
 			// aapt resource value: 0x7f060006
-			public const int menu_settings = 2131099654;
+			public const int emailInput = 2131099654;
+			
+			// aapt resource value: 0x7f060008
+			public const int loginButton = 2131099656;
 			
 			// aapt resource value: 0x7f060005
-			public const int textView = 2131099653;
+			public const int loginText = 2131099653;
+			
+			// aapt resource value: 0x7f06000c
+			public const int menu_settings = 2131099660;
+			
+			// aapt resource value: 0x7f06000a
+			public const int noAccText = 2131099658;
+			
+			// aapt resource value: 0x7f060007
+			public const int passwordInput = 2131099655;
+			
+			// aapt resource value: 0x7f060009
+			public const int signupButton = 2131099657;
+			
+			// aapt resource value: 0x7f06000b
+			public const int textView = 2131099659;
 			
 			// aapt resource value: 0x7f060000
 			public const int txtAccount = 2131099648;
-
-			public const int emailInput = 2131099649;
-			
-			// aapt resource value: 0x7f060003
-			public const int loginButton = 2131099651;
-			
-			// aapt resource value: 0x7f060000
-			public const int loginText = 2131099648;
-			
-			// aapt resource value: 0x7f060007
-			public const int menu_settings = 2131099655;
-			
-			// aapt resource value: 0x7f060005
-			public const int noAccText = 2131099653;
-			
-			// aapt resource value: 0x7f060002
-			public const int passwordInput = 2131099650;
-			
-			// aapt resource value: 0x7f060004
-			public const int signupButton = 2131099652;
-			
-			// aapt resource value: 0x7f060006
-			public const int textView = 2131099654;
 			
 			static Id()
 			{
@@ -122,14 +116,13 @@ namespace nfc_app
 		{
 			
 			// aapt resource value: 0x7f030000
-
 			public const int Account = 2130903040;
-      
-			public const int Login = 2130903040;
-
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int Login = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Main = 2130903042;
 			
 			static Layout()
 			{
